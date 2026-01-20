4d2f74f725aa7036e9b817cfd4d1bb72d126f02b
SD-Duy.Nguyen Anh <duy.nguyen1@trginternational.com>
2026-01-20 11:20:40 +0700

    Create Model - Book

--- BookManagement/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ BookManagement/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02bfed8dc1ce01ca20bbb2bb60bbc2d4a7aba7d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
