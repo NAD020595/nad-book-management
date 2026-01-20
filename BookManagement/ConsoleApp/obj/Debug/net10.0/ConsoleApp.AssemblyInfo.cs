fcc14a137a2cc5fb8c4c919aa3f256d555f9874e
SD-Duy.Nguyen Anh <duy.nguyen1@trginternational.com>
2026-01-20 11:32:26 +0700

    Create method - BookSingleView

--- BookManagement/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ BookManagement/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d2f74f725aa7036e9b817cfd4d1bb72d126f02b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+feb666e36bfe89b2eefba7862913f8dcfe74540c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
