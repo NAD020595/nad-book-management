0908677a38096a0c0fcf1266cfdd4e6dc8790f8c
SD-Duy.Nguyen Anh <duy.nguyen1@trginternational.com>
2026-01-30 17:04:02 +0700

    Update Router class

--- BookManagement/ConsoleApp/Program.cs
+++ BookManagement/ConsoleApp/Program.cs
@@ -1,29 +1,41 @@
 ﻿using ConsoleApp.Controllers;
 using ConsoleApp.DataServices;
+using ConsoleApp.Framework;
 
-SimpleDataAccess context = new SimpleDataAccess();
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var context = new SimpleDataAccess();
 BookController controller = new BookController(context);
+
+Router.Instance.Register("about", About);
+Router.Instance.Register("help", Help);
 
 while (true)
 {
-    Console.Write("Request> ");
+    ViewHelp.Write("# Request >>> ", ConsoleColor.Green);
     string request = Console.ReadLine();
-    switch (request.ToLower())
+
+    Router.Instance.Forward(request);
+
+    Console.WriteLine();
+}
+
+static void About(Parameter parameter)
+{
+    ViewHelp.WriteLine("BOOK MANAGER version 1.0", ConsoleColor.Green);
+    ViewHelp.WriteLine("by NAD020595", ConsoleColor.Magenta);
+}
+
+static void Help(Parameter parameter)
+{
+    if (parameter == null)
     {
-        case "single":
-            controller.Single(1);
-            break;
-        case "create":
-            controller.Create();
-            break;
-        case "update":
-            controller.Update(1);
-            break;
-        case "list":
-            controller.List();
-            break;
-        default:
-            Console.WriteLine("Unknown command");
-            break;
+        ViewHelp.WriteLine("SUPPORTED COMMANDS:", ConsoleColor.Green);
+        ViewHelp.WriteLine(Router.Instance.GetRoutes(), ConsoleColor.Yellow);
+        ViewHelp.WriteLine("type: help ? cmd= <command> to get command details", ConsoleColor.Cyan);
+        return;
     }
+
+    Console.BackgroundColor = ConsoleColor.DarkBlue;
+    var command = parameter["cmd"].ToLower();
+    ViewHelp.WriteLine(Router.Instance.GetHelp(command));
 }
--- BookManagement/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
+++ BookManagement/ConsoleApp/obj/Debug/net10.0/ConsoleApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a142fbff2ee06c791d1b34375eab8fb122fe797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7537e1287f6bdda3e07c73bee868d1e67442d3d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
